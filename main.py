4748a07f55d2cbeb908239744e6bdef7601c21b5
Al3xPhys <109749416+Al3xPhys@users.noreply.github.com>
2024-03-07 17:22:52 +0000

    Added stage options menu
    Stage options menu allows user to change and set the max x,y and z range for position, speed, acceleration and jerk.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 import serial
 import serial.tools.list_ports
 
+
+
 class MainUI(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainUI, self).__init__()  # Call the inherited classes __init__ method
@@ -19,6 +21,7 @@ class MainUI(QtWidgets.QMainWindow):
                                                                                           self.yPosSpinBox.value()))
         self.setStageHeightButton.clicked.connect(lambda: self.stageController.set_stage_height(self.zPosSpinBox.value()))
         self.getStagePositionButton.clicked.connect(self.stageController.get_stage_pos)
+        self.actionChange_Max_Position_Values.triggered.connect(self.stageController.set_max_stage_position)
 
         try:
             ports = serial.tools.list_ports.comports()
@@ -32,8 +35,6 @@ class MainUI(QtWidgets.QMainWindow):
             error_dialog.setText("ERROR: Could not populate COM port list")
             error_dialog.exec()
         return
-
-
 
 class stageControl:
     def __init__(self):
@@ -97,6 +98,27 @@ class stageControl:
             error_dialog = QtWidgets.QErrorMessage(window)
             error_dialog.showMessage(str(error))
 
+    def set_max_stage_position(self):
+        self.stage_options = stage_options()
+
+class stage_options(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        super(stage_options, self).__init__()  # Call the inherited classes __init__ method
+        uic.loadUi('stage_options.ui', self)  # Load the .ui file
+        self.show()
+
+    def apply_position_changes(self):
+        return
+
+    def apply_speed_changes(self):
+        return
+
+    def apply_acceleration_changes(self):
+        return
+
+    def apply_jerk_changes(self):
+        return
 
 
 app = QtWidgets.QApplication(sys.argv)  # Create an instance of QtWidgets.QApplication
