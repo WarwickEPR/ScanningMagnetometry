1dd4998ceee1110563b5045bed74764542f8de4b
Al3xPhys <109749416+Al3xPhys@users.noreply.github.com>
2024-03-07 16:48:27 +0000

    added stage position labels and update function
    currently the labels update on a button click for testing but will remove button later and link label update to movement of stage

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ class MainUI(QtWidgets.QMainWindow):
 
         self.stageController = stageControl()  # stage controller class instance
 
+
+
         self.connectStageButton.clicked.connect(lambda: self.stageController.connect_stage(self.comPortBox.currentText()))
+        self.homeStageButton.clicked.connect(self.stageController.home_stage)
+        self.setPositionButton.clicked.connect(lambda: self.stageController.set_stage_pos(self.xPosSpinBox.value(),
+                                                                                          self.yPosSpinBox.value()))
+        self.setStageHeightButton.clicked.connect(lambda: self.stageController.set_stage_height(self.zPosSpinBox.value()))
+        self.getStagePositionButton.clicked.connect(self.stageController.get_stage_pos)
 
         try:
             ports = serial.tools.list_ports.comports()
@@ -28,7 +35,7 @@ class MainUI(QtWidgets.QMainWindow):
 
 
 
-class stageControl(QtWidgets.QMainWindow):
+class stageControl:
     def __init__(self):
         super(stageControl, self).__init__()
         self.ser = None
@@ -40,17 +47,29 @@ class stageControl(QtWidgets.QMainWindow):
         except:
             print("ERROR: Could not execute stage command")
 
+    def read_gcode(self, command):
+        try:
+            self.ser.write(f'{command}\r\n'.encode())
+            response = self.ser.readline()
+            self.ser.readline() #clears next line
+        except Exception as error:
+            error_dialog = QtWidgets.QErrorMessage(window)
+            error_dialog.showMessage(str(error))
+        return response
+
+
     def connect_stage(self, com_port, baud_rate=115200):
         try:
             # connect to stage code here
             self.ser = serial.Serial(port=com_port, baudrate=baud_rate)
-            msg = QtWidgets.QMessageBox(self)
+            msg = QtWidgets.QMessageBox(window)
             msg.setText("Connected Successful to: " + str(com_port))
             msg.exec()
         except Exception as error:
-            error_dialog = QtWidgets.QErrorMessage(self)
+            error_dialog = QtWidgets.QErrorMessage(window)
             error_dialog.showMessage(str(error))
-x
+
+
     def home_stage(self):
         self.execute_gcode('G28')  # home gcode
         return
@@ -64,6 +83,20 @@ x
         print(z)
         self.execute_gcode(f'G00 Z{z}')
         return
+
+    def get_stage_pos(self):
+        try:
+            response = self.read_gcode('M114')
+
+            response = response.decode("utf-8").split()  # response[0] = xPos in mm, [1] = yPos, [2] = zPos
+            xPos, yPos, zPos = response[0], response[1], response[2]
+            window.currentXLabel.setText(xPos)
+            window.currentYLabel.setText(yPos)
+            window.currentHeightLabel.setText(zPos)
+        except Exception as error:
+            error_dialog = QtWidgets.QErrorMessage(window)
+            error_dialog.showMessage(str(error))
+
 
 
 app = QtWidgets.QApplication(sys.argv)  # Create an instance of QtWidgets.QApplication
